6d873d05a68930644d9d42e81dac7b9799992cb6
Kim Wittenburg <codello@wittenburg.kim>
2020-04-13 13:02:46 +0200

    Improve CI
    Actions for Flake8 and MyPy
    Add flake8-isort, flake8-black

--- tasks.py
+++ tasks.py
@@ -2,6 +2,24 @@ from invoke import Context, task
 
 
 @task
+def lint(c):
+    c: Context
+    print("Running flake8...")
+    c.run("flake8")
+    print("Running MyPy...")
+    c.run("mypy motor_odm", pty=True)
+
+
+@task
+def format(c):
+    c: Context
+    print("Formatting with Black...")
+    c.run("black motor_odm", pty=True)
+    print("Sorting Imports...")
+    c.run("isort -rc", pty=True)
+
+
+@task
 def test(c, coverage=False):
     c: Context
     args = ["pytest"]
@@ -11,67 +29,15 @@ def test(c, coverage=False):
 
 
 @task
-def doctest(c):
-    c: Context
-    c.run("pytest --doctest-modules --doctest-continue-on-failure motor_odm", pty=True)
-
-
-@task
-def black(c, check=False):
-    c: Context
-    if check:
-        c.run("black motor_odm --check", pty=True)
-    else:
-        c.run("black motor_odm", pty=True)
-
-
-@task
-def flake8(c):
-    c: Context
-    c.run("flake8", pty=True)
-
-
-@task
-def mypy(c):
-    c: Context
-    c.run("mypy motor_odm", pty=True)
-
-
-@task
-def isort(c, check=False):
-    c: Context
-    if check:
-        c.run("isort -rc --check-only", pty=True)
-    else:
-        c.run("isort -rc", pty=True)
-
-
-@task
-def format(c):
-    c: Context
-    print("Formatting with Black...")
-    black(c)
-    print("Sorting Imports...")
-    isort(c)
-
-
-@task
-def lint(c):
-    c: Context
-    print("Running Black...")
-    black(c, check=True)
-    print("Running flake8...")
-    flake8(c)
-    print("Running MyPy...")
-    mypy(c)
-    print("Running isort...")
-    isort(c, check=True)
-
-
-@task
 def build_docs(c):
     c: Context
     c.run("cd docs && make html", pty=True)
+
+
+@task
+def doctest(c):
+    c: Context
+    c.run("pytest --doctest-modules --doctest-continue-on-failure motor_odm", pty=True)
 
 
 @task
